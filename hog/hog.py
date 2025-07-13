cd14ba3c5a1d9eb4fe1731e21986501504d51ef5
errorlike <errorlikeman@gmail.com>
2025-07-13 14:06:57 +0800

    problem 10 达到门槛分数使用boar策略

--- hog/hog.py
+++ hog/hog.py
@@ -341,7 +341,11 @@ def boar_strategy(score, opponent_score, threshold=11, num_rolls=6):
     points, and returns NUM_ROLLS otherwise. Ignore the Sus Fuss rule.
     """
     # BEGIN PROBLEM 10
-    return num_rolls  # Remove this line once implemented.
+    inc_score = boar_brawl(score, opponent_score)
+    if inc_score >= threshold:
+        return 0
+    else:
+        return num_rolls
     # END PROBLEM 10
 
 
